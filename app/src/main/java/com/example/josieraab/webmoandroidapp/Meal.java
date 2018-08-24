ee512d2c19ff9d2da793437533a581e7624f85ea
Guenay <guenay.dogan@gmx.de>
2018-08-24 11:57:46 +0200

    Login

--- app/src/main/java/com/example/josieraab/webmoandroidapp/Meal.java
+++ app/src/main/java/com/example/josieraab/webmoandroidapp/Meal.java
@@ -2,40 +2,97 @@ package com.example.josieraab.webmoandroidapp;
 
 public class Meal {
 
-        private int mealId;
+    private int mealId;
+    private String name;
+    private float price;
+    private MealType mealType;
+
+    public int getMealId() {
+        return mealId;
+    }
+
+    public void setMealId(int mealId) {
+        this.mealId = mealId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public MealType getMealType() {
+        return mealType;
+    }
+
+    public void setMealType(MealType mealType) {
+        this.mealType = mealType;
+    }
+
+    private int mealId;
         private String name;
         private float price;
         private MealType mealType;
 
-        public Meal(int MealId) {
-            this.mealId = MealId;
-        }
 
-        public int getMealId() {
-            return mealId;
-        }
+    Meal meal = new Meal(1);
+    create.meal.setName("@string/kindofmeal1");
 
-        public String getName() {
-            return name;
-        }
 
-        public void setName(String name) {
-            this.name = name;
-        }
 
-        public float getPrice() {
-            return price;
-        }
 
-        public void setPrice(float price) {
-            this.price = price;
-        }
-         public MealType getMealType() {
-            return mealType;
-         }
+          /*Meal meal = new Meal(2);
+    create()meal.setName("@String/kindofmeal2");
+    create()meal.setPrice(22,50f);
+    create()meal.setMealType("@String/kindofmeal2art");
 
-          public void setMealType(MealType  mealType) {
-             this.mealType = mealType;
-          }
-    }
+    Meal meal = new Meal(3);
+    create()meal.setName("@String/kindofmeal3");
+    create()meal.setPrice(3,01f);
+    create()meal.setMealType("@String/kindofmeal3art");
+
+
+    Meal meal = new Meal(4);
+    create()meal.setName("Spagehtti");
+    create()meal.setPrice(3,01f);
+    create()meal.setMealType("vegan");
+
+
+    Meal meal = new Meal(5);
+    create()meal.setName("Spagehtti");
+    create()meal.setPrice(3,01f);
+    create()meal.setMealType("vegan");
+    Meal meal = new Meal(1);
+    create()meal.setName("Spagehtti");
+    create()meal.setPrice(3,01f);
+    create()meal.setMealType("vegan");
+    Meal meal = new Meal(1);
+    create()meal.setName("Spagehtti");
+    create()meal.setPrice(3,01f);
+    create()meal.setMealType("vegan");
+    Meal meal = new Meal(1);
+    create()meal.setName("Spagehtti");
+    create()meal.setPrice(3,01f);
+    create()meal.setMealType("vegan");
+    Meal meal = new Meal(1);
+    create()meal.setName("Spagehtti");
+    create()meal.setPrice(3,01f);
+    create()meal.setMealType("vegan");
+    Meal meal = new Meal(1);
+    create()meal.setName("Spagehtti");
+    create()meal.setPrice(3,01f);
+    create()meal.setMealType("vegan"); */
+
+
+}
 
